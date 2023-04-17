368613f21d648fdc9feb725da595f021c32523a5
Emmanuel Musiiwa <G20m4048@ict.ru.ac.za>
2023-04-17 14:31:44 +0200

    Added Task 4 to SimpleFunctions.java file

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,21 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static List<Integer> Task4(int[] a, int[] b) {
+        if (a.length != b.length) {                                 // Check if the lengths of the lists are the same
+            return null;
+        }
+
+        List<Integer> results = new ArrayList<>();                  // Initialize an empty list to hold the result
+        // Loop over the length of a
+        for (int i = 0; i < a.length; i++) {
+            int product = a[i] * b[b.length - i - 1];               // Calculate the product of the corresponding elements of a and b
+            results.add(product);                                   // Add the product to the results ArrayList
+        }
+         return results;
+    }
+
+
 }
+
