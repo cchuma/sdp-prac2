c87c7d83078694f2da7613b201b7538e5c4606b9
Emmanuel Musiiwa <G20m4048@ict.ru.ac.za>
2023-04-17 19:38:29 +0200

    Added tests for Task 5

--- app/src/test/java/sdp/prac2/AppTest.java
+++ app/src/test/java/sdp/prac2/AppTest.java
@@ -62,4 +62,21 @@ public class Task6Test {
     }
 }
 
+
+    public static void tests_Task5() {
+        // Arrange: Create input data and expected results
+        int[] sortedList = {1, 2, 3, 4, 5};
+        int[] unsortedList = {5, 2, 3, 4, 1};
+        
+        // Act: Call the function under test
+        boolean isSortedResult1 = Task5(sortedList);
+        boolean isSortedResult2 = Task5(unsortedList);
+        
+        // Assert: Check the results against the expected values
+        assert isSortedResult1 == true : "Expected true but got false";
+        assert isSortedResult2 == false : "Expected false but got true";
+    }
+
+
+
 }
