97f933dc016c99a8a20902de619bf71dc73c67e2
EtienneNoir <etiennebanza37@gmail.com>
2023-04-17 14:47:07 +0200

    Fixed the Task 5 function

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -9,13 +9,13 @@ public class SimpleFunctions {
 
     
     public Boolean Task5( List<Integer> items){
-        if (list == null || list.size() < 2) {
+        if (items == null || items.size() < 2) {
             // True is returned if a list has zero or one item in it, as a list with one or zero items does not need to be sorted. It is already in a sorted state 
             return true;
         }
         
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i) < list.get(i - 1)) {
+        for (int i = 1; i < items.size(); i++) {
+            if (items.get(i) < items.get(i - 1)) {
                 // / Traversing the list, checking if the current item is less than the preivous item , if true then it means that the list is not sorted
                 return false;
             }
